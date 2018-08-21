02c035a089adca96681251d380c8c2ac1d51dadf
SurrealShock <Adrian_Avram@Outlook.com>
2018-08-21 10:11:55 -0700

    Sidebar added

--- src/App.js
+++ src/App.js
@@ -11,13 +11,21 @@ class App extends Component {
     return (
       <Provider store={store}>
         <div className="App" style={{ color: 'black' }}>
-          <div
-            className="rounded m-5 float-left pb-3"
-            style={{
-              boxShadow: '0 0 20px rgba(0,0,0,0.10), 0 6px 6px rgba(0,0,0,0.15)'
-            }}
-          >
-            <PostList />
+          <div class="row">
+            <div class="col-4 col-sm-2">
+              <SideBar />
+            </div>
+            <div class="col-7 col-sm-8 content">
+              <div
+                className="rounded m-5 float-left pb-3"
+                style={{
+                  boxShadow:
+                    '0 0 20px rgba(0,0,0,0.10), 0 6px 6px rgba(0,0,0,0.15)'
+                }}
+              >
+                <PostList />
+              </div>
+            </div>
           </div>
         </div>
       </Provider>
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,53 +1,46 @@
 import React, { Component } from 'react';
 
 class SideBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: 0, height: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
+
   render() {
     return (
-      <div className="mt-5 mb-3" id="sidemenu">
-        <ul className="nav flex-md-column flex-row justify-content-between">
-          <li className="nav-item">
-            <a href="#sec1" className="nav-link active pl-0">
-              One
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#sec2" className="nav-link pl-0">
-              Two
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#sec3" className="nav-link pl-0">
-              Three
-            </a>
-            <ul className="nav flex-md-column ml-2 hidden-sm-down">
-              <li className="nav-item">
-                <a href="#sec3a" className="nav-link">
-                  Section A
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#sec3b" className="nav-link">
-                  Section B
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li className="nav-item">
-            <a href="#sec4" className="nav-link pl-0">
-              Four
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#sec5" className="nav-link pl-0">
-              Five
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#sec6" className="nav-link pl-0">
-              Six
-            </a>
-          </li>
-        </ul>
+      <div
+        class="menu sticky-top p-3 shadow"
+        style={{ height: this.state.height }}
+      >
+        <h2>
+          <i style={{ color: '#ff4500' }} class="fab fa-reddit" /> redd
+          <span style={{ color: '#ff4500' }}>i</span>t
+        </h2>
+        <div class="nav flex-column">
+          <a href="#" class="nav-link pl-0">
+            Menu 1
+          </a>
+          <a href="#" class="nav-link pl-0">
+            Menu 2
+          </a>
+          <a href="#" class="nav-link pl-0">
+            Menu 3
+          </a>
+        </div>
       </div>
     );
   }
