78cc37841f2a1fee6a786864a4939296c6491872
SurrealShock <Adrian_Avram@Outlook.com>
2018-08-21 09:27:06 -0700

    Temporarily removed navbar and sidebar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from './store';
 import PostList from './components/PostList';
+import SideBar from './components/SideBar';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
+        <div className="App" style={{ color: 'black' }}>
           <div
-            className="rounded m-5 float-left"
+            className="rounded m-5 float-left pb-3"
             style={{
               boxShadow: '0 0 20px rgba(0,0,0,0.10), 0 6px 6px rgba(0,0,0,0.15)'
             }}
