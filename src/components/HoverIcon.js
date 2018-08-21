fea54bf3cf24cca3fdaf65c45f8a4189f5ad3885
SurrealShock <Adrian_Avram@Outlook.com>
2018-08-21 09:27:37 -0700

    Add hover effect for vote buttons

---
+++ src/components/HoverIcon.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+export default class SocialIcon extends Component {
+  state = {
+    hover: false
+  };
+
+  onMouseUpdate = () => {
+    this.setState({
+      hover: !this.state.hover
+    });
+  };
+
+  render() {
+    const { className, color, size } = this.props;
+
+    return (
+      <i
+        className={className}
+        style={
+          this.state.hover
+            ? { cursor: 'pointer', fontSize: size, color: color }
+            : { cursor: 'pointer', fontSize: size }
+        }
+        onMouseEnter={this.onMouseUpdate}
+        onMouseLeave={this.onMouseUpdate}
+      />
+    );
+  }
+}
+
+SocialIcon.defaultProps = {
+  size: '1em',
+  color: '#FF6F00'
+};
